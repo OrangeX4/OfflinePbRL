4730a1475edb2db50626269f2946d096e128a06e
OrangeX4 <orangex4@qq.com>
2025-06-11 15:54:38 +0800

    update: complete basic cprl

--- offlinepbrl/policy/model_free/cprl.py
+++ offlinepbrl/policy/model_free/cprl.py
@@ -145,24 +145,23 @@ class CPRLPolicy(SACPolicy):
             .repeat(1, self._num_repeat_actions, 1) \
             .view(batch_size * self._num_repeat_actions, next_obss.shape[-1])
         
-        # update actor with both original and sampled observations
-        all_obss = torch.cat([obss, tmp_obss])
-        a_all, log_probs_all = self.actforward(all_obss)
-        q1a_all, q2a_all = self.critic1(all_obss, a_all), self.critic2(all_obss, a_all)
-        actor_loss = (self._alpha * log_probs_all - torch.min(q1a_all, q2a_all)).mean()
+        # update actor - only on original observations
+        a, log_probs = self.actforward(obss)
+        q1a, q2a = self.critic1(obss, a), self.critic2(obss, a)
+        actor_loss = (self._alpha * log_probs - torch.min(q1a, q2a)).mean()
         self.actor_optim.zero_grad()
         actor_loss.backward()
         self.actor_optim.step()
 
         if self._is_auto_alpha:
-            log_probs = log_probs_all[:batch_size].detach() + self._target_entropy
+            log_probs = log_probs.detach() + self._target_entropy
             alpha_loss = -(self._log_alpha * log_probs).mean()
             self.alpha_optim.zero_grad()
             alpha_loss.backward()
             self.alpha_optim.step()
             self._alpha = self._log_alpha.detach().exp()
         
-        # compute td error
+        # compute td error for critic update
         if self._max_q_backup:
             with torch.no_grad():
                 tmp_next_obss_backup = next_obss.unsqueeze(1) \
@@ -188,6 +187,7 @@ class CPRLPolicy(SACPolicy):
 
         target_q = rewards + self._gamma * (1 - terminals) * next_q
         
+        # Conservative loss for reward model - using Q = reward + gamma * V
         obs_reward_value = self.calc_reward_values(tmp_obss, tmp_obss, tmp_next_obss)
         next_obs_reward_value = self.calc_reward_values(tmp_next_obss, tmp_obss, tmp_next_obss)
         random_reward_value = self.calc_random_reward_values(tmp_obss, random_actions, tmp_next_obss)
@@ -199,7 +199,7 @@ class CPRLPolicy(SACPolicy):
         cat_q_pred = torch.cat([obs_reward_value, next_obs_reward_value, random_reward_value], 1)
         q_pred = self.reward_model(obss, actions) + self._gamma * (1 - terminals) * next_q.detach()
 
-        conservative_loss = \
+        reward_conservative_loss = \
             torch.logsumexp(cat_q_pred / self._temperature, dim=1).mean() * self._cql_weight * self._temperature - \
             q_pred.mean() * self._cql_weight
         
@@ -220,15 +220,15 @@ class CPRLPolicy(SACPolicy):
         reg_loss = (reward_1.square().mean() + reward_2.square().mean()) / 2
         
         # Total reward model loss
-        reward_model_loss = pref_loss + self.reward_reg * reg_loss + conservative_loss
+        reward_model_loss = pref_loss + self.reward_reg * reg_loss + reward_conservative_loss
         
         if self._with_lagrange:
             cql_alpha = torch.clamp(self.cql_log_alpha.exp(), 0.0, 1e6)
-            conservative_loss_scaled = cql_alpha * (conservative_loss - self._lagrange_threshold)
-            reward_model_loss = pref_loss + self.reward_reg * reg_loss + conservative_loss_scaled
+            reward_conservative_loss_scaled = cql_alpha * (reward_conservative_loss - self._lagrange_threshold)
+            reward_model_loss = pref_loss + self.reward_reg * reg_loss + reward_conservative_loss_scaled
 
             self.cql_alpha_optim.zero_grad()
-            cql_alpha_loss = -conservative_loss_scaled
+            cql_alpha_loss = -reward_conservative_loss_scaled
             cql_alpha_loss.backward(retain_graph=True)
             self.cql_alpha_optim.step()
         
@@ -236,57 +236,41 @@ class CPRLPolicy(SACPolicy):
         reward_model_loss.backward()
         self.reward_model_optim.step()
 
-        # Update critics with both original data and sampled actions from conservative computation
-        # Get the sampled actions that were used in conservative loss computation
-        with torch.no_grad():
-            sampled_obs_actions, _ = self.actforward(tmp_obss)
-            sampled_next_obs_actions, _ = self.actforward(tmp_next_obss)
+        # CQL-style conservative loss for Q networks
+        obs_pi_value1, obs_pi_value2 = self.calc_pi_values(tmp_obss, tmp_obss)
+        next_obs_pi_value1, next_obs_pi_value2 = self.calc_pi_values(tmp_next_obss, tmp_obss)
+        random_value1, random_value2 = self.calc_random_values(tmp_obss, random_actions)
+
+        obs_pi_value1 = obs_pi_value1.reshape(batch_size, self._num_repeat_actions, 1)
+        obs_pi_value2 = obs_pi_value2.reshape(batch_size, self._num_repeat_actions, 1)
+        next_obs_pi_value1 = next_obs_pi_value1.reshape(batch_size, self._num_repeat_actions, 1)
+        next_obs_pi_value2 = next_obs_pi_value2.reshape(batch_size, self._num_repeat_actions, 1)
+        random_value1 = random_value1.reshape(batch_size, self._num_repeat_actions, 1)
+        random_value2 = random_value2.reshape(batch_size, self._num_repeat_actions, 1)
         
-        # Compute targets for sampled actions
-        with torch.no_grad():
-            sampled_next_v_obs = torch.min(
-                self.critic1_old(tmp_next_obss, sampled_obs_actions),
-                self.critic2_old(tmp_next_obss, sampled_obs_actions)
-            )
-            sampled_next_v_next = torch.min(
-                self.critic1_old(tmp_next_obss, sampled_next_obs_actions),
-                self.critic2_old(tmp_next_obss, sampled_next_obs_actions)
-            )
-            random_next_v = torch.min(
-                self.critic1_old(tmp_next_obss, random_actions),
-                self.critic2_old(tmp_next_obss, random_actions)
-            )
-            
-            # Targets for sampled actions using reward model
-            sampled_target_obs = self.reward_model(tmp_obss, sampled_obs_actions) + self._gamma * sampled_next_v_obs
-            sampled_target_next = self.reward_model(tmp_obss, sampled_next_obs_actions) + self._gamma * sampled_next_v_next
-            sampled_target_random = self.reward_model(tmp_obss, random_actions) + self._gamma * random_next_v
-        
-        # Compute Q values for original data
+        # cat_q shape: (batch_size, 3 * num_repeat, 1)
+        cat_q1 = torch.cat([obs_pi_value1, next_obs_pi_value1, random_value1], 1)
+        cat_q2 = torch.cat([obs_pi_value2, next_obs_pi_value2, random_value2], 1)
+
+        # Update critics - only on original data with conservative loss
         q1, q2 = self.critic1(obss, actions), self.critic2(obss, actions)
         critic1_loss = ((q1 - target_q).pow(2)).mean()
         critic2_loss = ((q2 - target_q).pow(2)).mean()
+
+        # Add CQL conservative loss to critics
+        conservative_loss1 = \
+            torch.logsumexp(cat_q1 / self._temperature, dim=1).mean() * self._cql_weight * self._temperature - \
+            q1.mean() * self._cql_weight
+        conservative_loss2 = \
+            torch.logsumexp(cat_q2 / self._temperature, dim=1).mean() * self._cql_weight * self._temperature - \
+            q2.mean() * self._cql_weight
         
-        # Add loss for sampled actions
-        q1_sampled_obs = self.critic1(tmp_obss, sampled_obs_actions)
-        q1_sampled_next = self.critic1(tmp_obss, sampled_next_obs_actions)
-        q1_random = self.critic1(tmp_obss, random_actions)
-        
-        q2_sampled_obs = self.critic2(tmp_obss, sampled_obs_actions)
-        q2_sampled_next = self.critic2(tmp_obss, sampled_next_obs_actions)
-        q2_random = self.critic2(tmp_obss, random_actions)
-        
-        critic1_loss += ((q1_sampled_obs - sampled_target_obs).pow(2)).mean()
-        critic1_loss += ((q1_sampled_next - sampled_target_next).pow(2)).mean()
-        critic1_loss += ((q1_random - sampled_target_random).pow(2)).mean()
-        
-        critic2_loss += ((q2_sampled_obs - sampled_target_obs).pow(2)).mean()
-        critic2_loss += ((q2_sampled_next - sampled_target_next).pow(2)).mean()
-        critic2_loss += ((q2_random - sampled_target_random).pow(2)).mean()
+        critic1_loss = critic1_loss + conservative_loss1
+        critic2_loss = critic2_loss + conservative_loss2
 
         # update critic
         self.critic1_optim.zero_grad()
-        critic1_loss.backward()
+        critic1_loss.backward(retain_graph=True)
         self.critic1_optim.step()
 
         self.critic2_optim.zero_grad()
@@ -307,10 +291,15 @@ class CPRLPolicy(SACPolicy):
             "loss/critic1": critic1_loss.item(),
             "loss/critic2": critic2_loss.item(),
             "loss/preference": pref_loss.item(),
-            "loss/conservative": conservative_loss.item(),
+            "loss/reward_conservative": reward_conservative_loss.item(),
+            "loss/q_conservative1": conservative_loss1.item(),
+            "loss/q_conservative2": conservative_loss2.item(),
             "loss/reward_model": reward_model_loss.item(),
             "loss/reg": reg_loss.item(),
             "misc/reward_acc": reward_accuracy.item(),
+            "misc/q1": q1.mean().item(),
+            "misc/q2": q2.mean().item(),
+            "misc/next_q": next_q.mean().item(),
             "info/reward_mean": rewards_all.mean().item(),
             "info/reward_std": rewards_all.std().item(),
             "info/reward_win_mean": rewards_win.mean().item(),
